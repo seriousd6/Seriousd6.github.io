48e345095e9ceef879fed40f1ae05d0d9f69f333
seriousd6 <50645790+seriousd6@users.noreply.github.com>
2020-10-04 16:58:03 -0400

    Add files via upload

---
+++ js/js/compliments.js
@@ -0,0 +1,118 @@
+//Chance and array manipulation methods
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+};
+
+function rollDice(number) {
+    result = (Math.floor(Math.random() * number))
+    return result;
+};
+
+function searchArray(array) {
+    let shuffled = shuffle(array)
+    return shuffled[Math.floor(Math.random() * shuffled.length)];
+};
+
+
+// convert numbers to word form
+function toWords(s) {
+    var th = ['', 'thousand', 'million', 'billion', 'trillion'];
+    var dg = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+    var tn = ['Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen'];
+    var tw = ['Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety'];
+
+
+    s = s.toString();
+    s = s.replace(/[\, ]/g, '');
+    if (s != parseFloat(s)) return 'not a number';
+    var x = s.indexOf('.');
+    if (x == -1)
+        x = s.length;
+    if (x > 15)
+        return 'too big';
+    var n = s.split('');
+    var str = '';
+    var sk = 0;
+    for (var i = 0; i < x; i++) {
+        if ((x - i) % 3 == 2) {
+            if (n[i] == '1') {
+                str += tn[Number(n[i + 1])] + ' ';
+                i++;
+                sk = 1;
+            } else if (n[i] != 0) {
+                str += tw[n[i] - 2] + ' ';
+                sk = 1;
+            }
+        } else if (n[i] != 0) { // 0235
+            str += dg[n[i]] + ' ';
+            if ((x - i) % 3 == 0) str += 'hundred ';
+            sk = 1;
+        }
+        if ((x - i) % 3 == 1) {
+            if (sk)
+                str += th[(x - i - 1) / 3] + ' ';
+            sk = 0;
+        }
+    }
+
+    if (x != s.length) {
+        var y = s.length;
+        str += 'point ';
+        for (var i = x + 1; i < y; i++)
+            str += dg[n[i]] + ' ';
+    }
+    return str.replace(/\s+/g, ' ');
+};
+//Reload Page
+function reload() {
+    location.reload()
+};
+
+
+let cornyComparison = [
+    "Rakshasa's socks",
+    "Crawling Claw's clap",
+    "Owlbear's hoot",
+    "Dryad's sprig",
+    "Unicorn's horn",
+    "Trant's trunk",
+    "Dragon's hoard",
+
+]
+let mid = [
+    "a creative",
+    "a gorgeous",
+    "a beautiful",
+    "an intelligent",
+    "a friendly",
+    "a kind",
+    "an awesome",
+    "an impressive",
+    "a great",
+    "an important",
+    "an admirable",
+    "a dependable",
+    "a talented",
+    "a hardworking",
+    "an imaginative",
+    "a generous",
+    "an excellent",
+]
+let finisher = [
+    "person",
+    "DM",
+    "friend",
+]
+let template = [
+    `It's cool to be in the presence of such ${searchArray(mid)} ${searchArray(finisher)}!`,
+    `You are as cool as rolling two natural twenties in a row!`,
+    `People are glad and lucky to know ${searchArray(mid)} ${searchArray(finisher)} like you!`,
+    `I love your creativity!`,
+    `I think you are the ${searchArray(cornyComparison)}`,
+
+]
+console.log(searchArray(template))
