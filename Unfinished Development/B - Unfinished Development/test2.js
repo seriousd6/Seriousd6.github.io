10eb439ffc3f5e519256b9aa9169c2e0221080e5
David Seis <85198545+David-Seis@users.noreply.github.com>
2022-09-02 01:48:22 -0400

    more updates

--- Unfinished Development/B - Unfinished Development/test2.js
+++ Unfinished Development/B - Unfinished Development/test2.js
@@ -1,3 +1,131 @@
-let a =[1,2,3]
-let [b] = a
-console.log(b)
+/*###################Chance and array manipulation methods#########################*/
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+};
+function searchArray(array) {
+    let shuffled = shuffle(array)
+    return shuffled[Math.floor(Math.random() * shuffled.length)];
+};
+function rollDice(number) {
+    result = (Math.floor(Math.random() * number))
+    return result;
+};
+function rollArray(array) {
+    let shuffled = shuffle(array)
+    let index = Math.floor(Math.random() * shuffled.length)
+    return `(Roll: ${index}/${shuffled.length}) ${shuffled[index]}`;
+};
+function variableEffect(array, array2, array3,array4) {
+    let chance = rollDice(100)
+    if (chance > 80) {
+        return 'No Special Effect'
+    } else if (chance > 60) {
+        return searchArray(array3)
+    } else if (chance > 40) {
+        return "Observing this artwork has caused " + searchArray(array2[0])
+    } else if (chance > 20){
+        return "If you " + searchArray(array) + ', then ' + searchArray(array2[1])
+    } else {
+        return `This artwork is cursed. ${searchArray(array4)}`
+    }
+};
+function variableEvent(givenArray, number) {
+    let chance = rollDice(100)
+    if (chance < 75) {
+        return ""
+    } else if (number === undefined) {
+        return searchArray(givenArray) + " "
+    } else {
+        return searchArray(givenArray[number]) + ' '
+    }
+};
+function printFrom(array, number, id) {
+    let list = shuffle(array).slice(0, number)
+    list.forEach(function(item) {
+        let li = document.createElement("li");
+        let text = document.createTextNode(item);
+        li.appendChild(text);
+        document.getElementById(id).appendChild(li);
+    });
+};
+function modify(number) {
+    return Math.floor(number * (.95 + Math.random() * .4))
+};
+function shuffleSlice(array, number) {
+    return shuffle(array).slice(0, number)
+};
+function loopCountPrintList(array, id) {
+
+    final = array
+    final.forEach(function(item) {
+        let li = document.createElement("li");
+        let text = document.createTextNode(item);
+        li.appendChild(text);
+        document.getElementById(id).appendChild(li);
+    });
+};
+function toWords(s) {
+    let th = ['', 'thousand', 'million', 'billion', 'trillion'];
+    let dg = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let tn = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let tw = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+
+    s = s.toString();
+    s = s.replace(/[\, ]/g, '');
+    if (s != parseFloat(s)) return 'not a number';
+    let x = s.indexOf('.');
+    if (x == -1)
+        x = s.length;
+    if (x > 15)
+        return 'too big';
+    let n = s.split('');
+    let str = '';
+    let sk = 0;
+    for (let i = 0; i < x; i++) {
+        if ((x - i) % 3 == 2) {
+            if (n[i] == '1') {
+                str += tn[Number(n[i + 1])] + ' ';
+                i++;
+                sk = 1;
+            } else if (n[i] != 0) {
+                str += tw[n[i] - 2] + ' ';
+                sk = 1;
+            }
+        } else if (n[i] != 0) { // 0235
+            str += dg[n[i]] + ' ';
+            if ((x - i) % 3 == 0) str += 'hundred ';
+            sk = 1;
+        }
+        if ((x - i) % 3 == 1) {
+            if (sk)
+                str += th[(x - i - 1) / 3] + ' ';
+            sk = 0;
+        }
+    }
+
+    if (x != s.length) {
+        let y = s.length;
+        str += 'point ';
+        for (let i = x + 1; i < y; i++)
+            str += dg[n[i]] + ' ';
+    }
+    return str.replace(/\s+/g, ' ');
+};
+function printArray(array) {
+    for (i = 0; i < array.length; i++) {
+        console.log(array[i])
+    }
+};
+function reload() {
+    location.reload()
+};
+
+/*==================================================================================*/
+/*-----------------------------Page Scripts Below-----------------------------------*/
+/*==================================================================================*/
+
