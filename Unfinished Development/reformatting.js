0308ca88870b602c845b06d76544b603eddf759f
seriousd6 <50645790+seriousd6@users.noreply.github.com>
2023-07-10 01:21:41 -0400

    roomlands bulk

---
+++ Unfinished Development/reformatting.js
@@ -0,0 +1,7 @@
+`TREASURE FALL - A stone capsule falls from the sky, triggered by your movement. Nothing attacks you, but the capsule contains a TREASURE within it. to your inventory - you may be able to trade it later on in your adventure.`,
+`ASTROLITHIC ROOK CHASE - An Astrolithic Rook falls, but something about it seems wrong; it falls onto a distant rooftop, gets up, and turns in your direction. You see its eyes flash - it’s hunting you! Describe how you run away and hide from it (using other prompts from this EXPLORATION phase), or turn to fight! (See below to create your opponent)`,
+`EERIE SILENCE - Nothing happens. Maybe you got away with it this time.`,
+`ASTROLITHIC WRECKFALL - draw another card to see if you are hit by debris from the falling wreck of a downed Astrolithic Rook. If it is RED you are hit and take one WOUND. If it is BLACK you are safe.`,
+`METEOR SHOWER - Tonnes of burning debris falls from the sky, perhaps once a Rook, but now just fiery balls of death. You’re caught out with nowhere to take cover, take one WOUND.`,
+`ASTROLITHIC ROOK APPEARS - See below to create your opponent and then fight!`
+
