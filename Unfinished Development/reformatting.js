2eb8f7bca46d00c0d3a5cfc850eaff8f0b90baf2
seriousd6 <50645790+seriousd6@users.noreply.github.com>
2023-08-02 22:16:52 -0400

    12

---
+++ Unfinished Development/reformatting.js
@@ -0,0 +1,17 @@
+
+
+
+"Chimera Claw Grapple: An extendable grapple mounted on the roof, tipped with a chimera claw, allowing the wagon to scale walls or cliffs when needed.",
+"Dragon Wing Extensions: Enchanted dragon wing extensions that deploy and flap, granting the wagon limited flight capabilities, allowing it to soar over obstacles and difficult terrains.",
+
+
+
+"Aurora Borealis Wings: Wings that unfurl from the sides of the roof, shimmering with the colors of the aurora borealis, granting the wagon the ability to traverse the skies like a celestial phenomenon.",
+"Glowing Guardian Totems: Totems of guardian spirits placed on the corners of the roof, emitting protective auras and blessings during dangerous encounters.",
+
+
+"Arcane Amplifier Array: An array of crystals and enchantments that amplify magical spells and artifacts within the wagon, empowering the adventurers' abilities.",
+
+"Enchanted Flying Drums: Drums integrated into the roof, played with rhythmic beats to generate a levitation field, allowing the wagon to fly for a short duration.",
+
+"Lightning Rod Harpoon: A retractable harpoon mounted on the roof, designed to harness the power of lightning for offensive or defensive purposes.",
